ba04cad97d05a6325e2fa05e19d28a5324da8516
yuzhiyong <yuzhiyong@ren001.com>
2016-05-03 19:04:05 +0800

    0503

---
+++ 0427/js/product.js
@@ -0,0 +1,109 @@
+/*
+ *  "productId"	: "1", 产品ID
+ *	"productName" : "家庭服务型机器人", 产品名称
+ *	"productPrice" : "9999", 产品价格
+ *	"productNo" : "EO1-FO1", 产品编号
+ *	"productModel" : "", 产品型号
+ *	"productImageUrl" : "" 产品图片
+ * 	"productbrief" : "" 产品介绍
+ */
+
+
+var product_json = {
+	"product_data" : [
+		{
+			"productId"	: "1",
+			"productName" : "家庭服务型机器人",
+			"productPrice" : "9999",
+			"productNo" : "EO1-No",
+			"productModel" : "EO1",
+			"productImageUrl" : "images/1.png",
+			"productBrief" : "",
+			"productPackage" : [{
+				"productPackageId":"1",
+				"productPackageName" : "机器人",
+				"productPackagePrice" : "",
+				"productPackageNo" : "EO1-P1-No",
+				"productPackageModel" : "EO1-P1",
+				"productPackageImageUrl" : "images/1.png",
+				"productPackageBrief" : ""
+			},{
+				"productPackageId":"2",
+				"productPackageName" : "充电桩",
+				"productPackagePrice" : "",
+				"productPackageNo" : "EO1-P2-No",
+				"productPackageModel" : "EO1-P2",
+				"productPackageImageUrl" : "images/1.png",
+				"productPackageBrief" : ""
+			},{
+				"productPackageId":"3",
+				"productPackageName" : "体感空鼠",
+				"productPackagePrice" : "",
+				"productPackageNo" : "EO1-P3-No",
+				"productPackageModel" : "EO1-P3",
+				"productPackageImageUrl" : "images/1.png",
+				"productPackageBrief" : ""
+			}]
+		},{
+			"productId"	: "2",
+			"productName" : "机器人",
+			"productPrice" : "11999",
+			"productNo" : "EO2-No",
+			"productModel" : "EO2",
+			"productImageUrl" : "images/1.png",
+			"productBrief" : "",
+			"productPackage" : []
+		}
+	],
+	"product_parts" : [
+		{
+			"productId"	: "1",
+			"productName" : "体感空鼠",
+			"productPrice" : "100",
+			"productNo" : "EO1-FO1-No",
+			"productModel" : "EO1-FO1-No",
+			"productImageUrl" : "images/1.png",
+			"productBrief" : "体感游戏必备"
+			
+		},
+		{
+			"productId"	: "2",
+			"productName" : "便捷线充",
+			"productPrice" : "100",
+			"productNo" : "EO1-FO2-No",
+			"productModel" : "EO1-FO2-No",
+			"productImageUrl" : "images/1.png",
+			"productBrief" : "应急必备"
+			
+		},
+		{
+			"productId"	: "3",
+			"productName" : "互动麦克",
+			"productPrice" : "100",
+			"productNo" : "EO1-FO3-No",
+			"productModel" : "EO1-FO3-No",
+			"productImageUrl" : "images/1.png",
+			"productBrief" : "公用场所必备"
+			
+		},
+		{
+			"productId"	: "4",
+			"productName" : "净化器滤网",
+			"productPrice" : "100",
+			"productNo" : "EO1-FO4-No",
+			"productModel" : "EO1-FO4-No",
+			"productImageUrl" : "images/1.png",
+			"productBrief" : "好家居必备"
+			
+		}
+	]
+}
+/*产品价格*/
+
+var product_money = {
+	product_m : "9999_|_11999",
+	parts_m : "100_|_100_|_100_|_100"
+}
+
+
+
